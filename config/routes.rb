8518568e085b7855a95404d2a6adccabfd28133e
omar <oashraf5@gmail.com>
2017-05-06 18:42:37 +0000

    Update 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'list/index'
-
+  
   root 'application#hello'
+  root 'application#new_todo'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
